951b91261b690fe5737c2d4b707ef48457f24cd1
parth.shah <parth.shah@msbc.in>
2022-07-19 18:30:15 +0530

    now news api data will be cached for 10 minutes.

--- blog/views.py
+++ blog/views.py
@@ -10,17 +10,21 @@ from django.http import JsonResponse
 from django.core.exceptions import ObjectDoesNotExist
 from blog.forms import SignUpForm
 from django.core.paginator import Paginator
+from django.core.cache import cache
 
 
 # Create your views here.
 def home(request):
+    trending_loaded = cache.get('trending_loaded')
+    if trending_loaded is None:
+        trending_loaded = requests.get('https://inshorts.deta.dev/news?category=national').json()['data'][:10]
+        cache.set('trending_loaded', trending_loaded, 600)
     all_posts = Blog.objects.filter(hidden=False).order_by('id')
     paginator = Paginator(all_posts, 10, orphans=3)
     page = request.GET.get('page')
     posts = paginator.get_page(page)
-    trending = []
     # trending = requests.get('https://inshorts.deta.dev/news?category=national').json()['data'][:18]
-    return render(request, 'index.html', context={'posts': posts, 'trending': trending})
+    return render(request, 'index.html', context={'posts': posts, 'trending': trending_loaded})
 
 
 def contact(request):
