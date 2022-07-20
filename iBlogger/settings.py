880b17a618b7d918381113ec173d5b54f96f00d4
parth.shah <parth.shah@msbc.in>
2022-07-20 12:25:16 +0530

    updated cache settings

--- iBlogger/settings.py
+++ iBlogger/settings.py
@@ -145,9 +145,9 @@ TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
-USE_TZ = True
-
 USE_L10N = True
+
+USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
