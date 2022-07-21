5a661fb3dc3efc237f2079c23ed915c87cd726fb
parth.shah <parth.shah@msbc.in>
2022-07-21 16:48:53 +0530

    added memcached for cache uses.

--- iBlogger/settings.py
+++ iBlogger/settings.py
@@ -193,8 +193,8 @@ SESSION_COOKIE_AGE = 300
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
-        'LOCATION': 'home_cache',
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+        'LOCATION': '127.0.0.1:11211',
     }
 }
 
