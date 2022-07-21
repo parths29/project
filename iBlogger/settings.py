cf9addc8fbbbd18c08a16cafe2c8a94b37af6a4c
parth.shah <parth.shah@msbc.in>
2022-07-21 18:19:02 +0530

    test for memcached username & password

--- iBlogger/settings.py
+++ iBlogger/settings.py
@@ -193,16 +193,8 @@ SESSION_COOKIE_AGE = 300
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
         'LOCATION': '127.0.0.1:11211',
-        'OPTIONS': {
-            'binary': True,
-            'username': 'admin',
-            'password': 'admin',
-            'behaviors': {
-                'ketama': True,
-            }
-        }
     }
 
 }
